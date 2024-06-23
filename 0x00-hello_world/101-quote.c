a59663861d1955d3e206e565d16fc15f47be13e0
ELmehdibro1995 <elmehdicool@gmail.com>
2024-06-23 16:11:22 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line,
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+	return (1);
+}
